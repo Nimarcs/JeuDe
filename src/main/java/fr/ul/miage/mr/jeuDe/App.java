22c66c6cc5cb8957690306db0f4937ee3c9b2e25
RICHIER Marcus <marcus.richier8@etu.univ-lorraine.fr>
2025-01-11 16:37:01 +0100

    [FIX] Enfin la solution

---
+++ src/main/java/fr/ul/miage/mr/jeuDe/App.java
@@ -0,0 +1,10 @@
+package fr.ul.miage.mr.jeuDe;
+
+import fr.ul.miage.mr.jeuDe.ui.MainApp;
+
+public class App {
+
+    public static void main(String[] args) {
+        MainApp.main(args);
+    }
+}
